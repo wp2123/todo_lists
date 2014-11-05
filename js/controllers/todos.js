8957d61273c832036b3bb042565744468a0e124a
wp2123 <wangpu2123@sina.com>
2014-11-05 23:58:45 +0800

    add todo

--- js/controllers/todos.js
+++ js/controllers/todos.js
@@ -1,3 +1,27 @@
 TodoList.TodosController = Ember.ArrayController.extend({
 
+    newTitle: null,
+
+    actions: {
+        createTodo: function () {
+            console.log("I'm creating Toto");
+            var title = this.get('newTitle');
+
+            if(!title) {
+                return false;
+            }
+            if(!title.trim()) {
+                return;
+            }
+
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            this.set('newTitle');
+
+            todo.save();
+        }
+    }
 });
